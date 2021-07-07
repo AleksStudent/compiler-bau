9198e965c32dc47390d6b1d662fe4e4a60342e52
Nikita Trotner <nikita@ttnr.me>
2021-07-07 16:00:22 +0200

    added if code generator

--- abstract_syntax/If.java
+++ abstract_syntax/If.java
@@ -1,3 +1,8 @@
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Label;
+
 public class If extends Stmt {
 
 	public Expr cond;
@@ -8,6 +13,39 @@ public class If extends Stmt {
 		this.cond = cond;
 		this.stmt = stmt;
 		this.optional = optional;
+	}
+	
+	public void codeGen(ClassWriter cw, MethodVisitor method) {
+		Label conditionExpression = new Label();
+		Label innerIfExpression = new Label();
+		
+		int selectedCode = 0;
+		switch (((Unary) cond).operator) {
+		case "==": selectedCode = Opcodes.IF_ICMPEQ; break;
+		case "!=": selectedCode = Opcodes.IF_ICMPNE; break;
+		case ">": selectedCode = Opcodes.IF_ICMPGT; break;
+		case ">=": selectedCode = Opcodes.IF_ICMPGE; break;
+		case "<": selectedCode = Opcodes.IF_ICMPLT; break;
+		case "<=": selectedCode = Opcodes.IF_ICMPLE; break;
+		}
+		this.stmt.codeGen(cw, method);
+
+		if (selectedCode == 0) {
+			if (((Unary) cond).operator == "&&") {
+				method.visitJumpInsn(Opcodes.IFEQ, conditionExpression);
+				this.optional.codeGen(cw, method);
+				method.visitJumpInsn(Opcodes.IFEQ, conditionExpression);
+				method.visitLabel(innerIfExpression);
+			} else if (((Unary) cond).operator == "||") {
+				method.visitJumpInsn(Opcodes.IFNE, conditionExpression);
+				this.optional.codeGen(cw, method);
+				method.visitJumpInsn(Opcodes.IFEQ, innerIfExpression);
+				method.visitLabel(conditionExpression);
+			}
+		} else {
+			method.visitJumpInsn(selectedCode, conditionExpression);
+			method.visitLabel(innerIfExpression);
+		}
 	}
 
 	@Override
--- abstract_syntax/Stmt.java
+++ abstract_syntax/Stmt.java
@@ -1,4 +1,10 @@
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+
 public class Stmt {
 
+	public void codeGen(ClassWriter cw, MethodVisitor method) {
+		
+	}
 
 }
