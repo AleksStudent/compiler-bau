8873f6028718691443ba13a877233d3a0d1efa26
Nikita Trotner <nikita@ttnr.me>
2021-07-08 14:31:33 +0200

    added codeGen for accessing variables of objects

--- abstract_syntax/InstVar.java
+++ abstract_syntax/InstVar.java
@@ -1,3 +1,9 @@
+import java.util.Vector;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
 public class InstVar extends Expr {
 
 	public Expr expr;
@@ -6,6 +12,18 @@ public class InstVar extends Expr {
 	public InstVar(final Expr expr, final String name) {
 		this.expr = expr;
 		this.name = name;
+	}
+	
+	public void codeGen(ClassWriter cw, MethodVisitor method, Class i_class, Vector<LocalVarDecl> localVar) {
+		this.expr.codeGen(cw, method, i_class, localVar);
+		Type typeOfAccessedObject;
+		
+		if (((LocalOrFieldVar) expr).isFieldVar(((LocalOrFieldVar) expr).name, i_class.fields)) {
+			typeOfAccessedObject = ((LocalOrFieldVar) expr).getFieldVar(((LocalOrFieldVar) expr).name, i_class.fields).type;
+		} else {
+			typeOfAccessedObject = ((LocalOrFieldVar) expr).getLocalVar(((LocalOrFieldVar) expr).name, localVar).type;
+		}
+		method.visitFieldInsn(Opcodes.GETFIELD, typeOfAccessedObject.getType(), this.name, null);
 	}
 
 	@Override
