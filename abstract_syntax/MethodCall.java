6695397bbab81e900b3549fba4c6fb11ef1c6a42
Nikita Trotner <nikita@ttnr.me>
2021-07-08 14:30:48 +0200

    added codeGen for calling a method

--- abstract_syntax/MethodCall.java
+++ abstract_syntax/MethodCall.java
@@ -1,5 +1,11 @@
 import java.util.Vector;
 
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+
+
 public class MethodCall extends StmtExpr {
 
 	public Expr expr;
@@ -10,6 +16,33 @@ public class MethodCall extends StmtExpr {
 		this.expr = expr;
 		this.name = name;
 		this.expressions = expressions;
+	}
+	
+	public void codeGen(ClassWriter cw, MethodVisitor method, Class i_class, Vector<LocalVarDecl> localVar) {
+		for (Expr expression: expressions) {
+			((LocalOrFieldVar) expression).codeGen(cw, method, i_class, localVar);
+		}
+		String parameterInput = "";
+		
+		for (Expr parameter: expressions) {
+			parameterInput += ((LocalOrFieldVar) parameter).getVariableType(((LocalOrFieldVar) parameter).name, i_class, localVar);
+		}
+		
+		Method foundMethod = null;
+		for (Method methodIt: i_class.methods) {
+			if (methodIt.name.str == this.name) foundMethod = methodIt;
+		}
+		
+		String methodSignature = "()V";
+		
+		if (foundMethod != null) {
+			methodSignature = "(" + parameterInput + ")" + foundMethod.returnType.getType();
+		} else {
+			methodSignature = "(" + parameterInput + ")" + Type.VOID_TYPE.getClassName();
+		}
+		
+		
+		method.visitMethodInsn(Opcodes.INVOKEVIRTUAL, i_class.name, this.name, methodSignature, false);
 	}
 
 	@Override
