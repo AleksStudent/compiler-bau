cdeaa0a34c9ae5ad5da548ba2560e9b5e6ce1270
s190845 <s190845@student.dhbw-mannheim.de>
2021-07-12 12:05:59 +0200

    add changes to this branch

--- Main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-    public static void main(String args[])
-                throws java.io.IOException {
-    scanner s =
-        new scanner (
-            new java.io.InputStreamReader(System.in));
-    while (s.yylex() != -1) {}
-    }
-}
