a28befac4b0b2012ed59bbd78696b2052a60391a
Nikita Trotner <nikita@ttnr.me>
2021-07-12 19:41:08 +0200

    added codeGen for Program and prototyped for New

--- New.java
+++ New.java
@@ -1,6 +1,10 @@
 import java.util.Map;
 import java.util.Vector;
 
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
 public class New extends StmtExpr {
 
 	public Type type;
@@ -9,6 +13,29 @@ public class New extends StmtExpr {
 	public New(final Type type, final Vector<Expr> expressions) {
 		this.type = type;
 		this.expressions = expressions;
+	}
+	
+	public void codeGen(ClassWriter cw, MethodVisitor method, Class i_class, Vector<LocalVarDecl> localVar) {
+		//method.visitTypeInsn(Opcodes.NEW, type.name);
+		method.visitInsn(Opcodes.DUP);
+		
+		for (Expr expression: expressions) {
+			expression.codeGen(cw, method, i_class, localVar);
+		}
+		
+		// String(String): String, Object(): void
+		String objectOperand = "()V";
+		String stringOperand = "(String)String";
+		String selectedOperand;
+		
+		/*
+		switch(type.name) {
+			case "String": selectedOperand = stringOperand; break;
+			case "Object": selectedOperand = objectOperand; break;
+		}
+
+		method.visitMethodInsn(Opcodes.INVOKESPECIAL, type.name, "<init>", selectedOperand, false);
+		*/
 	}
 
 	@Override
--- Program.java
+++ Program.java
@@ -1,9 +1,28 @@
+import java.io.IOException;
+
+import org.objectweb.asm.ClassWriter;
+
 public class Program {
 
 	public Class clazz;
 
 	public Program(final Class clazz) {
 		this.clazz = clazz;
+	}
+	
+	public void codeGen(Program i_program) {
+		// define class writer
+		ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+		
+		i_program.clazz.codeGen(cw, i_program.clazz);
+		
+		try {
+			BytecodeGenerator.writeClassfile(cw);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 	@Override
