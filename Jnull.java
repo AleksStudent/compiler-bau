871dc05eb6cccddb66798fad6407d5e866e1b159
Aleksn41 <Aleksn41@gmail.com>
2021-07-14 00:13:56 +0200

    standardized and made some minor fixes for codegen

--- Jnull.java
+++ Jnull.java
@@ -7,18 +7,20 @@ import org.objectweb.asm.Opcodes;
 
 public class Jnull extends Expr {
 
-	public void codeGen(ClassWriter cw, MethodVisitor method) {
-		method.visitInsn(Opcodes.ACONST_NULL);
-		System.out.println("[Null] Writing: null");
-	}
+    public Type type = Type.TYPE_NULL;
 
-	public Object getValue() {
-		return null;
-	}
+    public void codeGen(ClassWriter cw, MethodVisitor method) {
+        method.visitInsn(Opcodes.ACONST_NULL);
+        System.out.println("[Null] Writing: null");
+    }
+
+    public Object getValue() {
+        return null;
+    }
 
     @Override
     public Type typeCheck(Map<String, Type> localVars, Class thisClass) {
-        return Type.TYPE_NULL;
+        return type;
     }
 
     @Override
@@ -26,9 +28,9 @@ public class Jnull extends Expr {
         return "null";
     }
 
-	@Override
-	public void codeGen(ClassWriter cw, MethodVisitor method, Class i_class, Vector<LocalVarDecl> localVars) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void codeGen(ClassWriter cw, MethodVisitor method, Class i_class, Vector<LocalVarDecl> localVars) {
+        // TODO Auto-generated method stub
+
+    }
 }
