a9fcec9d6a484d5fa6c5e5ea832ad07793944b23
Nikita Trotner <nikita@ttnr.me>
2021-07-13 21:08:13 +0200

    added codeGen

--- TypeCheckable.java
+++ TypeCheckable.java
@@ -1,5 +1,10 @@
 import java.util.Map;
+import java.util.Vector;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
 
 public interface TypeCheckable {
 	Type typeCheck(Map<String,Type> localVars, Class thisClass);
+	void codeGen(ClassWriter cw, MethodVisitor method, Class i_class, Vector<LocalVarDecl> localVars);
 }
