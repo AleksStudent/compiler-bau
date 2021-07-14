d5001e9630264f2ed6ce17e8a82c4d00e34a0a61
DominikJemmings <dominik.jemmings@yahoo.de>
2021-07-14 16:54:08 +0200

    add test-class again

--- Test.java
+++ Test.java
@@ -1,5 +1,50 @@
 class Test{
 
-	
+	int myNum;
+	String myText;
+	boolean myBool;
+
+
+	int main(int test, boolean test2) {
+
+		String test5;
+		int test6;
+		int test7;
+		String test8;
+		String test9;
+		String test10;
+
+		test5 = "sss";
+		test6 = 1;
+		test7 = 2 + 3;
+		test8 = new String("abc");
+		test9 = this.test("abc");
+		test10 = this.myNum;
+		test11 = test("abc");
+
+		if (test3) {
+
+		} else {
+
+		}
+
+		if (!test2) {
+
+		}
+
+		return 2;
+	}
+
+	int test() {
+
+		boolean test4;
+
+		while (test4 <= test4 && !test4) {
+
+		}
+
+	int Test() {
+		return 2;
+	}
 
 }
